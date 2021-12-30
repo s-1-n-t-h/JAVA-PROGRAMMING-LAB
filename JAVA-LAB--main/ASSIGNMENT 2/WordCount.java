a0e2472ca87d0356ca02baa209f7ecef1708bda9
s-1-n-t-h <b.v.s.yaswanth1@gmail.com>
2021-12-30 23:06:06 +0530

    q4

---
+++ JAVA-LAB--main/ASSIGNMENT 2/WordCount.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class WordCount {
+    int CountChar(String s)
+    {
+        return s.length();
+    }
+
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter String: ");
+        
+        WordCount wC = new WordCount();
+        String S = new String();
+        S = sc.nextLine();
+        System.out.println();
+        
+        System.out.print("No.of Characters: "+wC.CountChar(S));
+    }
+}
