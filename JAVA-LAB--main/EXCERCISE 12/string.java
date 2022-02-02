a8239c3975d2d317d5366509b034599febd90dcc
s-1-n-t-h <b.v.s.yaswanth1@gmail.com>
2022-02-02 20:23:17 +0530

    ADDED 12 C

---
+++ JAVA-LAB--main/EXCERCISE 12/string.java
@@ -0,0 +1,10 @@
+import STR.StrRev;
+public class string {
+    public static void main(String[] args)
+    {
+        StrRev s = new StrRev();
+
+        s.reverseString();
+        System.out.println();
+    }
+}
